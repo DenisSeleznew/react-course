a25ca566abfbaf8d9dcd092df8ce733509f70136
DenisSeleznev <generalpokrichkin@icloud.com>
2023-04-23 02:09:52 +0300

    create context

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,20 @@ import { ErrorMessage } from "./component/ErrorMassage";
 import { Loader } from "./component/Loader";
 import { Product } from "./component/Product";
 import { useProducts } from "./hooks/products";
+import { Modal } from "./component/Modal";
+import { CreateProduct } from "./component/CreateProduct";
+import { useContext } from "react";
+import { IProduct } from "./models";
+import { ModalContext } from "./context/ModalContext";
 
 function App() {
-	const { loading, error, products } = useProducts();
+	const { loading, error, products, addProduct } = useProducts();
+	const { modal, open, close } = useContext(ModalContext);
+
+	const createHandler = (product: IProduct) => {
+		close();
+		addProduct(product);
+	};
 
 	return (
 		<div className="container mx-auto max-w-2xl pt-5">
@@ -13,6 +24,19 @@ function App() {
 			{products.map(product => (
 				<Product product={product} key={product.id} />
 			))}
+
+			{modal && (
+				<Modal title="Create new product" onClose={close}>
+					<CreateProduct onCreate={createHandler} />
+				</Modal>
+			)}
+
+			<button
+				className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
+				onClick={open}
+			>
+				+
+			</button>
 		</div>
 	);
 }
