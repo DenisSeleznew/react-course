d17ba214eefcc07a87110861f593f1985f914fe2
DenisSeleznev <generalpokrichkin@icloud.com>
2023-04-22 20:55:03 +0300

    add hoks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ErrorMessage } from "./component/ErrorMassage";
+import { Loader } from "./component/Loader";
+import { Product } from "./component/Product";
+import { useProducts } from "./hooks/products";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { loading, error, products } = useProducts();
+
+	return (
+		<div className="container mx-auto max-w-2xl pt-5">
+			{loading && <Loader />}
+			{error && <ErrorMessage error={error} />}
+			{products.map(product => (
+				<Product product={product} key={product.id} />
+			))}
+		</div>
+	);
 }
 
 export default App;
