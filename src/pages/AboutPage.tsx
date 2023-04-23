437a8875fbd78bc7d5172462d53c15b3fd7ada3f
DenisSeleznev <generalpokrichkin@icloud.com>
2023-04-23 03:15:10 +0300

    project finish

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function AboutPage() {
+	return (
+		<div>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga eveniet
+			reprehenderit, itaque doloremque, temporibus aliquam eius aperiam quos
+			facilis soluta excepturi aliquid obcaecati? Labore dignissimos, ratione ex
+			id quis pariatur.
+		</div>
+	);
+}
